73b4a46fa67966126cfc2f02f0f9d8a0cbe64138
Goran Mitrasinovic <goran.mitrasinovic@dynabyte.se>
2019-08-20 13:58:46 +0200

    seeded real users for future auth

--- app/User.php
+++ app/User.php
@@ -13,7 +13,23 @@ class User extends Model
      * @var array
      */
     protected $fillable = [
-        'name',
+        'name', 'email', 'password',
+    ];
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
     ];
 
     public function competitions(){
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,4 @@
 
 Route::get('/{any}', 'SpaController@index')->where('any', '.*');
 
-Auth::routes();
-
 Route::get('/home', 'HomeController@index')->name('home');
